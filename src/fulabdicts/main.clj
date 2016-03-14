380f1246997bb7cd476ef059a89d406193f2ad0e
andrewboltachev <andrewboltachev@gmail.com>
2016-03-14 11:26:36 +0300

    depreciation channel

--- src/fulabdicts/main.clj
+++ src/fulabdicts/main.clj
@@ -8,16 +8,28 @@
     )
   )
 
-(def c (chan
-         (sliding-buffer 10)
+(def input-file-ch (chan
+         (sliding-buffer 1)
          ))
 
-(go-loop []
-         (let [v (<! c)]
-           (println v (quot (System/currentTimeMillis) 1000)
-                    )
+(go-loop [standby true]
+         (let [[v port] (alts! [input-file-ch
+                                (timeout 200)
+                                ]
+                               :priority true
+                               )]
+           (when (not standby)
+             #_(println
+                 (str "!!!!!!!!!!!!!!!!!!!!!!!!!!!!" v port)
+                 (System/currentTimeMillis)
+               )
+              (when
+                  (nil? v)
+                  (println "will send")
+                )
+             )
+           (recur (nil? v))
            )
-         (recur)
   )
 
 
@@ -25,11 +37,13 @@
   ; check exists (.exists (io/file "filename.txt"))
   (let [file (io/file input-file)]
   (start-watch [{:path (.getParent file)
-                 :event-types [:create :modify :delete]
+                 :event-types [:create :modify
+                               ;:delete
+                               ]
                  :bootstrap (fn [path] (println "Starting to watch " path))
                  :callback (fn [event filename]
                              (when (= (.getName file) (.getName (io/file filename)))
-                               (put! c [event filename])
+                               (put! input-file-ch [event filename])
                              )
                              )
                  :options {:recursive true}}])
